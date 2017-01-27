d4267ff689988740c91bb2e46c4dca6903fc8119
Liam Kelly <me@liamckelly.com>
2017-01-26 19:11:57 -0500

    Added card titles

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,34 +1,34 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+  <main class="main" style="width: 70%; margin: auto;" role="main">
 
-    <h3>Contact Information<h3>
-    <p style="font-size: .7em;">If you are interested in working with us, or just want to say hello, send us a message or give us a call.</p>
+    <h3 style="margin-left: 20%;">Contact Information<h3>
+    <p style="font-size: .7em; margin-left: 20%;">If you are interested in working with us, or just want to say hello, send us a message or give us a call.</p>
 
     <div style="display: block; text-align:center">
       <div class="contact_pic_column">
-        <div id="contact_info">
+        <!-- <div id="contact_info">
           <p>Janet Kelly</p>
           <p>978-857-5212</p>
           <p>Stow, MA</p>
-        </div>
+        </div> -->
         <div class="contact_pic_container">
           <img id="contact_pic" src="<?= image('chair.jpg')->url() ?>"  alt="Showroom"/>
       </div>
       </div>
 
       <div class="contact_text_column">
-        <div>
-          <h4 style="margin: 1.5rem 0 0rem;">Contact Form</h4>
+        <div style="float:left; width: 100%; ">
+          <h4 style="margin: 0.7rem 0 0rem;">Contact Form</h4>
         <form action="mailto:JanetKelly87@gmail.com" method="post" enctype="text/plain">
         Name:<br>
         <input class="text-box" type="text" name="name"><br>
         E-mail:<br>
-        <input class="text-box" type="text" name="mail"><br>
+        <input class="text-box"  type="text" name="mail"><br>
         Comment:<br>
-        <input class="text-box" type="text" name="comment" size="50"><br><br>
-        <input class="button" type="submit" value="Send">
-        <input class="button" type="reset" value="Reset">
+        <textarea class="text-box" id="comment-box" type="text" name="comment" size="50"></textarea><br><br>
+        <input class="button" id="submit" type="submit" value="Send">
+        <input class="button" id="reset" type="reset" value="Reset">
         </form>
       </div>
       </div>
